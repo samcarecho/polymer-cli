4c4f36aff651131e121635737292771108617eb7
Fred K. Schott <fschott@google.com>
2016-07-22 15:11:06 -0700

    Use polymer-build library for service-worker generation

--- src/build/build.ts
+++ src/build/build.ts
@@ -24,21 +24,11 @@ const cssSlam = require('css-slam').gulp;
 const htmlmin = require('gulp-html-minifier');
 
 import {ProjectConfig} from '../project-config';
-import {OptimizeOptions} from './optimize';
 import {PrefetchTransform} from './prefetch';
-import {waitForAll} from './streams';
+import {waitFor} from './streams';
 import {generateServiceWorker, parsePreCacheConfig, SWConfig} from './sw-precache';
 
 let logger = logging.getLogger('cli.build.build');
-
-export interface DepsIndex {
-  depsToFragments: Map<string, string[]>;
-  // TODO(garlicnation): Remove this map.
-  // A legacy map from framents to html dependencies.
-  fragmentToDeps: Map<string, string[]>;
-  // A map from frament urls to html, js, and css dependencies.
-  fragmentToFullDeps: Map<string, DocumentDeps>;
-}
 
 export interface BuildOptions {
   sources?: string[];
@@ -119,29 +109,14 @@ export function build(options?: BuildOptions, config?: ProjectConfig): Promise<a
       .pipe(polymerProject.bundle)
       .pipe(gulp.dest('build/bundled'));
 
-
-    let genSW = (buildRoot: string, deps: string[], swConfig: SWConfig, scriptAndStyleDeps?: string[]) => {
-      logger.debug(`Generating service worker for ${buildRoot}...`);
-      logger.debug(`Script and style deps: ${scriptAndStyleDeps}`);
-      return generateServiceWorker({
-        root: polymerProject.root,
-        entrypoint: polymerProject.entrypoint,
-        deps,
-        scriptAndStyleDeps,
-        buildRoot,
-        swConfig: clone(swConfig),
-        serviceWorkerPath: path.join(polymerProject.root, buildRoot, serviceWorkerName),
-      });
-    };
-
     let swPrecacheConfig = path.resolve(polymerProject.root, options.swPrecacheConfig || 'sw-precache-config.js');
+
     return Promise.all([
       parsePreCacheConfig(swPrecacheConfig),
-      polymerProject.analyze.analyzeDependencies,
-      waitForAll([unbundledPhase, bundledPhase])
+      waitFor(unbundledPhase),
+      waitFor(bundledPhase),
     ]).then((results) => {
       let swConfig: SWConfig = results[0];
-      let depsIndex: DepsIndex = results[1];
 
       if (swConfig) {
         logger.debug(`Service worker config found`, swConfig);
@@ -149,21 +124,15 @@ export function build(options?: BuildOptions, config?: ProjectConfig): Promise<a
         logger.debug(`No service worker configuration found at ${swPrecacheConfig}, continuing with defaults`);
       }
 
-      let unbundledDeps = polymerProject.analyze.allFragments
-          .concat(Array.from(depsIndex.depsToFragments.keys()));
-      let bundledDeps = polymerProject.analyze.allFragments
-          .concat(polymerProject.bundle.sharedBundleUrl);
-      let fullDeps = Array.from(depsIndex.fragmentToFullDeps.values());
-      let scriptAndStyleDeps = new Set<string>();
-      fullDeps.forEach(d => {
-        d.scripts.forEach((s) => scriptAndStyleDeps.add(s));
-        d.styles.forEach((s) => scriptAndStyleDeps.add(s));
-      });
-
       logger.info(`Generating service workers...`);
       return Promise.all([
-        genSW('build/unbundled', unbundledDeps, swConfig, Array.from(scriptAndStyleDeps)),
-        genSW('build/bundled', bundledDeps, swConfig)
+        polymerProject.addServiceWorker('build/unbundled', {
+          swConfig: swConfig,
+        }),
+        polymerProject.addServiceWorker('build/bundled', {
+          swConfig: swConfig,
+          bundled: true,
+        })
       ]);
     })
     .then(() => {
